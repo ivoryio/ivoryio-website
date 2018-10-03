854cb5ab1dc5709ee6531f64fec30b1d83029bd6
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-03 15:40:17 +0300

    first commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/Oana/Desktop/ivoryio-website/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/Oana/Desktop/ivoryio-website/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]

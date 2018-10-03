87173260058b538a7fdddc948be73e65fcdef3d8
Iulian Nechifor <iulian.nechifor@thinslices.com>
2018-10-03 13:15:15 +0300

    added stuff to text (for testing)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi there people</h1>
+    <p>Welcome to your new cool Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>

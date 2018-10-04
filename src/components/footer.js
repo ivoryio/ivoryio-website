e90b0b9fdd90ee4ab070c5c2cd2a8318c31133b4
Silv94 <silviu.popovici94@gmail.com>
2018-10-04 14:04:07 +0300

    stuff

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+export default class Footer extends Component {
+  render () {
+    return (
+      <FooterContainer>
+    
+      </FooterContainer>
+    )
+  }
+}
+const FooterContainer = styled.div`
+  width:100%;
+  height: 384px;
+  background-color: #394952;
+  
+`
+

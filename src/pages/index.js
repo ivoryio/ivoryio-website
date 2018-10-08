49973e6376959fbf22ed851d631f837502b0ab1f
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-08 17:51:43 +0300

    fix margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Presentation from '../components/ivoryPresentation'
-import LaunchingSoon from '../components/launchingSoon'
-import Footer from '../components/footer'
-import styled from 'styled-components'
-import Header from '../components/header'
 const IndexPage = () => (
   <div>
-    {/* <Header /> */}
     <Presentation />
-    {/* <LaunchingSoon /> */}
-    {/* <Footer /> */}
   </div>
 )
 

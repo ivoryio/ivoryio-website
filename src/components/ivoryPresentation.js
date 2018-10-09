fe926350179d62a7250b33795330e49bf0a64cf6
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-09 10:38:02 +0300

    refactor css

--- src/components/ivoryPresentation.js
+++ src/components/ivoryPresentation.js
@@ -24,7 +24,7 @@ export default class Presentation extends Component {
           Lorem ipsum dolor sit amet, tantas cetero option cu cum. <br />Tu esse debet vim, facete peopriae adversarium id qui, has in quodsi tractatos. <br />Te duo semper offendit, ne deseruisse necessitatibus conclusionemque nec. Et dolore consequat <br />usu. His prima dicunt detraxit ut.
         </p>
         <Grid >
-          <Row className='d-flex justify-content-around'>
+          <Row >
             <Col className={'column'} sm={12} md={4} >
               <img className={'image'} src={database} />
               <h3 className={'title'} > {constants.DATABASE_TITLE}</h3>
@@ -35,13 +35,13 @@ export default class Presentation extends Component {
               <h3 className={'title'}> {constants.SPEED_TITLE}</h3>
               <p className={'text'}>{constants.SPEED_TEXT}</p>
             </Col>
-            <Col className={'lastColumn'} sm={12} md={4} >
+            <Col className={'column'} sm={12} md={4} >
               <img className={'image'} src={machineLearning} />
               <h3 className={'title'}> {constants.MACHINE_LEARNING_TITLE}</h3>
               <p className={'text'}>{constants.MACHINE_LEARNING_TEXT}</p>
             </Col>
           </Row>
-          <Row className='d-flex justify-content-around'>
+          <Row>
             <Col className={'column'} sm={12} md={4} >
               <img className={'image'} src={secure} />
               <h3 className={'title'}> {constants.SECURE_TITLE}</h3>
@@ -52,13 +52,13 @@ export default class Presentation extends Component {
               <h3 className={'title'}> {constants.BLOCKCHAIN_TITLE}</h3>
               <p className={'text'}>{constants.BLOCKCHAIN_TEXT}</p>
             </Col>
-            <Col className={'lastColumn'} sm={12} md={4} >
+            <Col className={'column'} sm={12} md={4} >
               <img className={'image'} src={devops} />
               <h3 className={'title'}> {constants.DEVOPS_TITLE}</h3>
               <p className={'text'}>{constants.DEVOPS_TEXT}</p>
             </Col>
           </Row>
-          <Row className='d-flex justify-content-around'>
+          <Row>
             <Col className={'column'} sm={12} md={4}>
               <img className={'image'} src={quality} />
               <h3 className={'title'}> {constants.QUALITY_TITLE}</h3>
@@ -69,7 +69,7 @@ export default class Presentation extends Component {
               <h3 className={'title'}> {constants.PODS_TITLE}</h3>
               <p className={'text'}>{constants.PODS_TEXT}</p>
             </Col>
-            <Col className={'lastColumn'} sm={12} md={4}>
+            <Col className={'column'} sm={12} md={4}>
               <img className={'image'} src={openSource} />
               <h3 className={'title'}> {constants.OPENSOURCE_TITLE}</h3>
               <p className={'text'}>{constants.OPENSOURCE_TEXT}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Presentation from '../components/ivoryPresentation'
-const IndexPage = () => (
-  <div>
-    <Presentation />
-  </div>
-)
 
+const IndexPage = () => (
+  <body style={{margin: 0, padding: 0}}>
+    <div>
+      <Presentation />
+    </div>
+  </body>
+)
 export default IndexPage

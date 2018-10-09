7f81d0d595c1d4e8bedb12d94ec9df5300ffe8d3
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-09 13:45:58 +0300

    deleted layout

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,9 +3,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/Oana/Desktop/ivoryio-website/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/Oana/Desktop/ivoryio-website/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/Oana/Desktop/ivoryio-website/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/Oana/Desktop/ivoryio-website/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+  "component---src-pages-index-js": () => import("/Users/Oana/Desktop/ivoryio-website/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
 exports.data = () => import("/Users/Oana/Desktop/ivoryio-website/.cache/data.json")
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,8 +4,6 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/Oana/Desktop/ivoryio-website/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/Oana/Desktop/ivoryio-website/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/Oana/Desktop/ivoryio-website/src/pages/index.js")),
-  "component---src-pages-page-2-js": preferDefault(require("/Users/Oana/Desktop/ivoryio-website/src/pages/page-2.js"))
+  "component---src-pages-index-js": preferDefault(require("/Users/Oana/Desktop/ivoryio-website/src/pages/index.js"))
 }
 

a71deaa1330c55462674118e2e5d74fa5df446fc
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-11 12:11:16 +0300

    added mailerLiteForm

--- src/components/launchingSoon.js
+++ src/components/launchingSoon.js
@@ -1,54 +1,42 @@
-import React, {Component} from 'react'
-import { Row, Grid, Col, FormControl, Button } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Row, Grid, Col, FormControl, Button } from "react-bootstrap";
 
-import '../customStyles/launchingSoonCSS.css'
-import constants from '../constants/constants'
+import "../customStyles/launchingSoonCSS.css";
+import constants from "../constants/constants";
 
 export default class LaunchingSoon extends Component {
-  render () {
+  render() {
     return (
-      <div className={'containerLaunching'}>
-        <Grid >
-          <Row >
-            <Col sm={12} md={12} >
-              <h1 className={'h1Style'}> {constants.LAUNCHING_SOON}</h1>
+      <div className={"containerLaunching"}>
+        <Grid>
+          <Row>
+            <Col sm={12} md={12}>
+              <h1 className={"h1Style"}> {constants.LAUNCHING_SOON}</h1>
             </Col>
           </Row>
-          <Row >
-            <Col sm={12} md={12} >
-              <h3 className={'h3Style'}>{constants.SIGN_UP}</h3>
+          <Row>
+            <Col sm={12} md={12}>
+              <h3 className={"h3Style"}>{constants.SIGN_UP}</h3>
             </Col>
           </Row>
-          <Row >
-            <Col sm={12} md={12} >
-              <p className={'pStyle'}> {constants.SMALL_TEXT}</p>
+          <Row>
+            <Col sm={12} md={12}>
+              <p className={"pStyle"}> {constants.SMALL_TEXT}</p>
             </Col>
           </Row>
-          <div className='d-flex justify-content-center'>
-            <Row >
-              <Col sm={12} md={4} >
-                <FormControl
-                  type='text'
-                  placeholder='Name'
-                  className={'inputMailing'}
+          <div className="d-flex justify-content-center">
+            <Row>
+              <Col sm={12} md={12}>
+                <div
+                  class="ml-form-embed"
+                  data-account="1292624:y2g6q7x8c8"
+                  data-form="1071240:d2n8x2"
                 />
-                <div style={{ marginBottom: 5 }} />
-              </Col>
-              <Col sm={12} md={4} >
-                <FormControl
-                  type='text'
-                  placeholder='Email address'
-                  className={'inputMailing'}
-                />
-                <div style={{ marginBottom: 6 }} />
-              </Col>
-              <Col sm={12} md={4} >
-                <Button className={'subscribeMailing'}>SUBSCRIBE</Button>
               </Col>
             </Row>
           </div>
         </Grid>
       </div>
-    )
+    );
   }
 }

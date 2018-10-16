d862895f85b0eb3a6243254be62f4d0e55c3b8bc
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-16 16:10:35 +0300

    description and team sections from business page

--- src/pages/businessPage.js
+++ src/pages/businessPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from '../components/businessesComponents/businessHeader'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import '../customStyles/businessStyle/businessHeader.css'
+import BusinessDescription from '../components/businessesComponents/businessDescription';
 const ForBusiness = () => (
-    <div>
-      <Header />
-    </div>
+  
+      <BusinessDescription />
+
   )
   export default ForBusiness
   

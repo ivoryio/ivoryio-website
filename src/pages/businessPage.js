3734ede6df5f8c7d632198dd33a4b807e6ec2441
Silv94 <silviu.popovici94@gmail.com>
2018-10-16 11:47:25 +0300

    fix alignment issues

--- src/pages/businessPage.js
+++ src/pages/businessPage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from '../components/businessesComponents/businessHeader'
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import '../customStyles/businessStyle/businessHeader.css'
 const ForBusiness = () => (
     <div>
-      <Header />
+      Placeholder
     </div>
   )
   export default ForBusiness

b45a87a18a1a0a0b47a315d23083cdb9469052cf
Silv94 <silviu.popovici94@gmail.com>
2018-10-16 11:49:34 +0300

    fix alignment issues

--- src/pages/businessPage.js
+++ src/pages/businessPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 import 'bootstrap/dist/css/bootstrap.css'
-import '../customStyles/businessStyle/businessHeader.css'
+
 const ForBusiness = () => (
     <div>
       Placeholder

ae9d0bc43585bd0bb775349815533575f6e7e153
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-16 17:32:02 +0300

    add form component

--- src/pages/businessPage.js
+++ src/pages/businessPage.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import BusinessHeader from '../components/businessesComponents/businessHeader'
 import BusinessDescription from '../components/businessesComponents/businessDescription'
+import BusinessForm from '../components/businessesComponents/businessForm'
 
 const ForBusiness = () => (
     <div>
       <BusinessHeader />
       <BusinessDescription />
+      <BusinessForm />
     </div>
   )
   export default ForBusiness

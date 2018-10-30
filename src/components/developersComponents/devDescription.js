ec62c6cd49d72208cae4e6c2cfc578b4fea51eb1
OanaMacsi <oana.macsimiuc.e@gmail.com>
2018-10-30 13:28:32 +0200

    add all components from devPage

--- src/components/developersComponents/devDescription.js
+++ src/components/developersComponents/devDescription.js
@@ -6,7 +6,12 @@ import "../../customStyles/fonts.css";
 // import "../../customStyles/developerStyle/developerProject.css";
 
 import DCONSTANTS from "../../constants/developerConstants";
-import devOpenSource from "../../images/dev_open_source.png";
+import devOpenSource from "../../images/community_img@3x.png";
+import pods from "../../images/pods_img@3x.png";
+import serverless from "../../images/serverless_img@3x.png";
+import security from "../../images/security_img@3x.png";
+import ai from "../../images/ai_img@3x.png";
+import blockchain from "../../images/blockchain_img@3x.png";
 
 export default class DeveloperDescription extends Component {
   render() {
@@ -15,7 +20,7 @@ export default class DeveloperDescription extends Component {
         <h3 className={"developerTitle"}>
           We're still building, but here's what we're aiming at:
         </h3>
-        <div style={{ display: "flex" }}>
+        <div style={{ display: "flex" }} >
           <div id="fixOnScroll">
             <p>Open source community</p>
           </div>
@@ -26,7 +31,7 @@ export default class DeveloperDescription extends Component {
                 <p className={"titleOnWhite"}>Open Source Community</p>
                 <p className={"devText"}>{DCONSTANTS.OPEN_SOURCE_TEXT}</p>
               </div>
-              <div id="blue" className={"divAlign"}>
+              <div id="blue">
                 <img className={"devImages"} src={devOpenSource} />
               </div>
             </div>
@@ -43,8 +48,8 @@ export default class DeveloperDescription extends Component {
               </div>
             </div>
             <div className="squareWrap">
-              <div id="white" className={"divAlign"}>
-                {/* <img className={"devImages"} src={devOpenSource} /> */}
+              <div id="white">
+                <img className={"devImages"} src={pods} />
               </div>
               <div id="blue" className={"divAlign"}>
                 <p className={"titleOnWhite devText"}>
@@ -64,7 +69,107 @@ export default class DeveloperDescription extends Component {
                   {DCONSTANTS.SERVERLESS_TEXT2}
                 </p>
               </div>
-              <div id="white" className={"divAlign"} />
+              <div id="white">
+              <img className={"devImages"} src={serverless} />
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="white" className={"divAlign"}>
+              <p className={"titleOnWhite"}>Speed is a first class citizen</p>
+                <p className={"devText"}>{DCONSTANTS.SPEED_TEXT_WHITE}</p>
+              </div>
+              <div id="blue" className={"divAlign"}>
+                <p className={"titleOnWhite devText"}>
+                  {DCONSTANTS.SPEED_TEXT_BLUE}
+                </p>
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="blue" className={"divAlign"}>
+                <p className={"titleOnWhite whiteDivAlign "}>
+                Security and privacy
+                </p>
+                <p className={"titleOnWhite devText"}>
+                  {DCONSTANTS.SECURITY_TEXT_BLUE}
+                </p>
+              </div>
+              <div id="white" className={"divAlign"}>
+              <p className={"devText"}>{DCONSTANTS.SECURITY_TEXT_WHITE1}</p>
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="white" className={"divAlign"}>
+                <p className={"devText"}>{DCONSTANTS.SECURITY_TEXT_WHITE2}</p>
+              </div>
+              <div id="blue" >
+              <img className={"devImages"} src={security} />
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="blue">
+              <img className={"devImages"} src={ai} />
+              </div>
+              <div id="white" className={"divAlign"}>
+              <p className={"titleOnWhite"}>Machine Learning and AI</p>
+                <p className={"devText"}>{DCONSTANTS.AI_TEXT_WHITE1}</p>
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="white" className={"divAlign"}>
+                <p className={"devText"}>{DCONSTANTS.AI_TEXT_WHITE2}</p>
+              </div>
+              <div id="blue" className={"divAlign"}>
+              <p className={"titleOnWhite whiteDivAlign "}>
+              Blockchain
+                </p>
+                <p className={"titleOnWhite devText"}>
+                  {DCONSTANTS.BLOCKCHAIN_TEXT_BLUE1}
+                </p>
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="blue" className={"divAlign"}>
+              <p className={"titleOnWhite devText"}>
+                  {DCONSTANTS.BLOCKCHAIN_TEXT_BLUE2}
+                </p>
+              </div>
+              <div id="white">
+              <img className={"devImages"} src={blockchain} />
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="white" className={"divAlign"}>
+                <p className={"devText"}>{DCONSTANTS.BLOCKCHAIN_TEXT_WHITE3}</p>
+              </div>
+              <div id="blue" className={"divAlign"}>
+              <p className={"titleOnWhite devText"}>{DCONSTANTS.BLOCKCHAIN_TEXT_BLUE4}</p>
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="blue" className={"divAlign"}>
+              <p className={"titleOnWhite whiteDivAlign "}>
+              Continuous Delivery & Zero DevOps
+                </p>
+                <p className={"titleOnWhite devText"}>
+                  {DCONSTANTS.DEVOPS_TEXT_BLUE}
+                </p>
+              </div>
+              <div id="white" className={"divAlign"}>
+              <p className={"devText"}>{DCONSTANTS.DEVOPS_TEXT_WHITE}</p>
+              </div>
+            </div>
+            <div className="squareWrap">
+              <div id="white" >
+              {/* <img className={"devImages"} src={ai} /> */}
+              </div>
+              <div id="blue" className={"divAlign"}>
+              <p className={"titleOnWhite whiteDivAlign "}>
+              Quality
+                </p>
+                <p className={"titleOnWhite devText"}>
+                  {DCONSTANTS.QUALITY_TEXT}
+                </p>
+              </div>
             </div>
           </div>
         </div>

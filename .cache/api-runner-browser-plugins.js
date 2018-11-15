d5ffb2d15a8cca438ef08bbed6433b2e34b64b70
Stefan Sarbu <stefan@thinslices.com>
2018-11-15 11:49:31 +0200

    removed .cache and node_modules folders

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/Oana/Desktop/ivoryio-website/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/Oana/Desktop/ivoryio-website/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
